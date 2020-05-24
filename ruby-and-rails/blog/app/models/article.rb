cf8cbdf2459b904549b61c8f5e5fe14897632f4a
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-05-23 20:34:20 -0600

    refactor and add authentication to delete comments or create articles

--- ruby-and-rails/blog/app/models/article.rb
+++ ruby-and-rails/blog/app/models/article.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
-  has_many :comments
-  validates :title, presence: true,
-                    length: { minimum: 5 }
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true, length: { minimum: 5 }
 end
--- ruby-and-rails/blog/app/models/comment.rb
+++ ruby-and-rails/blog/app/models/comment.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
 class Comment < ApplicationRecord
   belongs_to :article
+  validates :commenter, presence: true, length: { minimum: 5 }
 end
