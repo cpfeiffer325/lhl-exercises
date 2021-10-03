183bd27f8164a166276e090515cf0bf76dbbc0ce
Christian Pfeiffer <christian.pfeiffer@zayzoon.com>
2021-10-03 15:01:57 -0600

    add key inputs for controlling snake

--- snake-game/snake-client/player-input.js
+++ snake-game/snake-client/player-input.js
@@ -1,4 +1,8 @@
-const setupInput = () => {
+let connection
+
+const setupInput = (conn) => {
+  connection = conn
+
   const stdin = process.stdin
   stdin.setRawMode(true)
   stdin.setEncoding("utf8")
@@ -10,9 +14,22 @@ const setupInput = () => {
 const handleUserInput = (key) => {
   const stdout = process.stdout
 
-  if (key === '\u0003') {
-    stdout.write("Exited snek game. Bye bye.\n")
-    process.exit();
+  switch (key) {
+    case '\u0003':
+      stdout.write("Exited snek game. Bye bye.\n")
+      process.exit()
+    case 'w':
+      connection.write("Move: up")
+      break
+    case 'a':
+      connection.write("Move: left")
+      break
+    case 's':
+      connection.write("Move: down")
+      break
+    case 'd':
+      connection.write("Move: right")
+      break
   }
 }
 
